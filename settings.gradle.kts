e80c55174568eaf16be86e686ff91566be3ae23c
Allan Taylor <allan.taylor.pi@gmail.com>
2026-08-01 02:58:17 +1200

    Sort mod versions ascending

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -29,25 +29,25 @@ stonecutter {
 		fun match(version: String, vararg loaders: String) =
 			loaders.forEach { version("$version-$it", version).buildscript = getBuildscript(it, version) }
 
-		match("26.2", "fabric")
-		match("26.1.2", "fabric", "neoforge")
-		match("1.21.7", "fabric", "neoforge")
-		match("1.21.1", "fabric", "neoforge")
-		match("1.21.11", "fabric")
-		match("1.21.10", "fabric")
-		match("1.21.5", "fabric")
-		match("1.21.4", "fabric")
-		match("1.21.3", "fabric")
-		match("1.20.6", "fabric")
-		match("1.20.4", "fabric")
-		match("1.20.2", "fabric")
-		match("1.20.1", "fabric")
-		match("1.18.2", "fabric")
+		// match("1.17.1", "fabric") /* fletching-table unsupported on JVM 16 */
 		match("1.18.1", "fabric")
-		match("1.19.4", "fabric")
-		match("1.19.3", "fabric")
+		match("1.18.2", "fabric")
 		match("1.19.2", "fabric", "forge")
-		// match("1.17.1", "fabric")
+		match("1.19.3", "fabric")
+		match("1.19.4", "fabric")
+		match("1.20.1", "fabric")
+		match("1.20.2", "fabric")
+		match("1.20.4", "fabric")
+		match("1.20.6", "fabric")
+		match("1.21.1", "fabric", "neoforge")
+		match("1.21.3", "fabric")
+		match("1.21.4", "fabric")
+		match("1.21.5", "fabric")
+		match("1.21.7", "fabric", "neoforge")
+		match("1.21.10", "fabric")
+		match("1.21.11", "fabric")
+		match("26.1.2", "fabric", "neoforge")
+		match("26.2", "fabric")
 
 		vcsVersion = "1.21.7-fabric"
 	}
